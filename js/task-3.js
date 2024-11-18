1d5bd2446c84143b9ae321070ac5066a3e5839dc
Anna <36787402+annavoy@users.noreply.github.com>
2024-11-18 16:48:22 +0000

    add js files

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,21 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername: function (newName) {
+    this.username = newName;
+  },
+  updatePlayTime: function (hours) {
+    this.playTime += hours;
+  },
+  getInfo: function () {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-  return totalWidth;
-}
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
